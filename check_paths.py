c29e736e13e0b0396dc84619a8853e5e4aa492e4
Julius <julius.simonelli@gmail.com>
2023-10-26 13:24:31 -0700

    Update check_paths.py

--- check_paths.py
+++ check_paths.py
@@ -5,46 +5,62 @@ PATH: When you activate a Conda environment, Conda will prepend the bin director
 
 LD_LIBRARY_PATH: Conda might adjust this variable to ensure that libraries specific to the activated environment are found before system-wide libraries. This is less common than the adjustments to PATH, but it can happen, especially when dealing with packages that have specific library dependencies that might conflict with system-wide installations.
 """
+import json
 import os
 import subprocess
 
 
 def get_env_var_from_system(var_name):
-    return os.environ.get(var_name, "")
+    value = os.environ.get(var_name, "")
+    print(f"[System] {var_name}: {value}")
+    return value
 
 
 def get_env_var_from_conda_env(env_name, var_name):
     command = f"conda activate {env_name} && echo ${{{var_name}}}"
-    result = subprocess.check_output(command, shell=True, executable="/bin/bash")
-    return result.decode().strip()
+    try:
+        result = subprocess.check_output(command, shell=True, executable="/bin/bash")
+        value = result.decode().strip()
+        print(f"[{env_name}] {var_name}: {value}")
+        return value
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command for environment {env_name}: {e}")
+        return ""
 
 
 def get_conda_envs():
     command = "conda env list --json"
-    result = subprocess.check_output(command, shell=True)
-    envs = result.decode().split("\n")
-    env_list = []
-    for line in envs:
-        if "name" in line:
-            env_name = line.split(":")[1].strip().replace('"', "")
-            env_list.append(env_name)
-    return env_list
+    try:
+        result = subprocess.check_output(command, shell=True)
+        data = json.loads(result.decode())
+        envs = data["envs"]
+        env_names = [env.split("/")[-1] for env in envs]
+        print(f"Found conda environments: {', '.join(env_names)}")
+        return env_names
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command to list conda environments: {e}")
+        return []
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON data: {e}")
+        return []
 
 
 def main():
     env_vars = ["PATH", "LD_LIBRARY_PATH"]
+    print("Fetching system-wide environment variables...")
     system_vals = {var: get_env_var_from_system(var) for var in env_vars}
 
+    print("\nFetching conda environments...")
     conda_envs = get_conda_envs()
 
     for env in conda_envs:
-        print(f"Checking Conda environment: {env}")
+        print(f"\nChecking Conda environment: {env}")
         for var in env_vars:
             conda_val = get_env_var_from_conda_env(env, var)
             if system_vals[var] != conda_val:
-                print(f"\033[91m{var} in {env}: {conda_val}\033[0m")  # Red text
+                print(f"\033[91mDifference detected in {var} for environment {env}\033[0m")  # Red text
             else:
-                print(f"{var} in {env}: {conda_val}")
+                print(f"{var} in {env} matches system value.")
         print("\n")
 
 
